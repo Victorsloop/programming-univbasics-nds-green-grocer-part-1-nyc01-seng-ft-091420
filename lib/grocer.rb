5fdfec7f0ebd6b958a8eb0b10f0163db3ea343c3
Victor Torres <torres.victor136@gmail.com>
2020-09-04 20:46:44 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-  
+  collection.each do |info|
+    if info[:item] == name
+      return info
+    end
+  end
+  nil
+end
 
 end
 
